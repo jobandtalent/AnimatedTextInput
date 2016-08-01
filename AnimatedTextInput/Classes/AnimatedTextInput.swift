b7f629e4640cf72a05fe720a7c5a7c901465ef0e
Victor Baro <fs.baro@gmail.com>
2016-08-01 12:01:42 -0700

    Add constraints without PureLayout

--- AnimatedTextInput/Classes/AnimatedTextInput.swift
+++ AnimatedTextInput/Classes/AnimatedTextInput.swift
@@ -1,5 +1,4 @@
 import UIKit
-import PureLayout
 
 @objc public protocol AnimatedTextInputDelegate: class {
 
@@ -47,8 +46,10 @@ public class AnimatedTextInput: UIControl {
     private let counterLabel = UILabel()
     private let lineWidth: CGFloat = 1
     private let counterLabelRightMargin: CGFloat = 15
+    private let counterLabelTopMargin: CGFloat = 5
 
     private var isPlaceholderAsHint = false
+    private var hasCounterLabel = false
     private var textInput: TextInput!
     private var textInputHeightConstraint: NSLayoutConstraint!
     private var placeholderErrorText = "Error message"
@@ -77,7 +78,77 @@ public class AnimatedTextInput: UIControl {
         return CGSize(width: UIViewNoIntrinsicMetric, height: normalHeight + style.topMargin + style.bottomMargin)
     }
 
+    public override func updateConstraints() {
+        addLineViewConstraints()
+        addTextInputConstraints()
+        super.updateConstraints()
+    }
+
     // MARK: Configuration
+
+    private func addLineViewConstraints() {
+        let leading = NSLayoutConstraint(item: lineView,
+                                         attribute: .Leading,
+                                         relatedBy: .Equal,
+                                         toItem: self,
+                                         attribute: .Leading,
+                                         multiplier: 1.0,
+                                         constant: styleConfigurator.leftMargin)
+        let trailing = NSLayoutConstraint(item: lineView,
+                                          attribute: .Trailing,
+                                          relatedBy: .Equal,
+                                          toItem: self,
+                                          attribute: .Trailing,
+                                          multiplier: 1.0,
+                                          constant: -styleConfigurator.rightMargin)
+        let height = NSLayoutConstraint(item: lineView,
+                                        attribute: .Height,
+                                        relatedBy: .Equal,
+                                        toItem: nil,
+                                        attribute: .NotAnAttribute,
+                                        multiplier: 1.0,
+                                        constant: lineWidth)
+        let constant = hasCounterLabel ? -counterLabel.intrinsicContentSize().height - counterLabelTopMargin : 0
+        lineToBottomConstraint = NSLayoutConstraint(item: lineView,
+                                                    attribute: .Bottom,
+                                                    relatedBy: .Equal,
+                                                    toItem: self,
+                                                    attribute: .Bottom,
+                                                    multiplier: 1.0,
+                                                    constant: constant)
+        addConstraints([leading, trailing, height, lineToBottomConstraint])
+    }
+    private func addTextInputConstraints() {
+        let leading = NSLayoutConstraint(item: textInput.view,
+                                         attribute: .Leading,
+                                         relatedBy: .Equal,
+                                         toItem: self,
+                                         attribute: .Leading,
+                                         multiplier: 1.0,
+                                         constant: styleConfigurator.leftMargin)
+        let trailing = NSLayoutConstraint(item: textInput.view,
+                                          attribute: .Trailing,
+                                          relatedBy: .Equal,
+                                          toItem: self,
+                                          attribute: .Trailing,
+                                          multiplier: 1.0,
+                                          constant: -styleConfigurator.rightMargin)
+        let top = NSLayoutConstraint(item: textInput.view,
+                                     attribute: .Top,
+                                     relatedBy: .Equal,
+                                     toItem: self,
+                                     attribute: .Top,
+                                     multiplier: 1.0,
+                                     constant: styleConfigurator.topMargin)
+        let bottom = NSLayoutConstraint(item: textInput.view,
+                                        attribute: .Bottom,
+                                        relatedBy: .Equal,
+                                        toItem: lineView,
+                                        attribute: .Top,
+                                        multiplier: 1.0,
+                                        constant: -styleConfigurator.bottomMargin)
+        addConstraints([leading, trailing, top, bottom])
+    }
 
     private func setupCommonElements() {
         addLine()
@@ -88,11 +159,8 @@ public class AnimatedTextInput: UIControl {
 
     private func addLine() {
         lineView.defaultColor = style.inactiveColor
+        lineView.translatesAutoresizingMaskIntoConstraints = false
         addSubview(lineView)
-        lineView.autoPinEdge(.Leading, toEdge: .Leading, ofView: self, withOffset: style.leftMargin)
-        lineView.autoPinEdge(.Trailing, toEdge: .Trailing, ofView: self, withOffset: -style.rightMargin)
-        lineToBottomConstraint = lineView.autoPinEdge(.Bottom, toEdge: .Bottom, ofView: self)
-        lineView.autoSetDimension(.Height, toSize: lineWidth)
     }
 
     private func addPlaceHolder() {
@@ -118,11 +186,8 @@ public class AnimatedTextInput: UIControl {
         textInput.view.tintColor = style.activeColor
         textInput.textColor = style.textInputFontColor
         textInput.font = style.textInputFont
+        textInput.view.translatesAutoresizingMaskIntoConstraints = false
         addSubview(textInput.view)
-        textInput.view.autoPinEdge(.Leading, toEdge: .Leading, ofView: self, withOffset: style.leftMargin)
-        textInput.view.autoPinEdge(.Trailing, toEdge: .Trailing, ofView: self, withOffset: -style.rightMargin)
-        textInput.view.autoPinEdge(.Bottom, toEdge: .Top, ofView: lineView, withOffset: -style.bottomMargin)
-        textInput.view.autoPinEdge(.Top, toEdge: .Top, ofView: self, withOffset: style.topMargin)
         invalidateIntrinsicContentSize()
     }
 
@@ -266,12 +331,28 @@ public class AnimatedTextInput: UIControl {
         counterLabel.text = "\(characters)/\(maximum)"
         counterLabel.textColor = isActive ? style.activeColor : style.inactiveColor
         counterLabel.font = style.counterLabelFont
+        counterLabel.translatesAutoresizingMaskIntoConstraints = false
         addSubview(counterLabel)
-        let topOffset: CGFloat = 5
-        counterLabel.autoPinEdge(.Top, toEdge: .Bottom, ofView: lineView, withOffset: topOffset)
-        counterLabel.autoPinEdge(.Trailing, toEdge: .Trailing, ofView: self, withOffset: -counterLabelRightMargin)
-        lineToBottomConstraint.constant = -counterLabel.intrinsicContentSize().height - topOffset
+        addCharacterCounterConstraints()
         invalidateIntrinsicContentSize()
+    }
+
+    private func addCharacterCounterConstraints() {
+        let top = NSLayoutConstraint(item: counterLabel,
+                                     attribute: .Top,
+                                     relatedBy: .Equal,
+                                     toItem: lineView,
+                                     attribute: .Bottom,
+                                     multiplier: 1.0,
+                                     constant: counterLabelTopMargin)
+        let trailing = NSLayoutConstraint(item: counterLabel,
+                                          attribute: .Trailing,
+                                          relatedBy: .Equal,
+                                          toItem: self,
+                                          attribute: .Trailing,
+                                          multiplier: 1.0,
+                                          constant: -counterLabelRightMargin)
+        addConstraints([top, trailing])
     }
 
     public func removeCharacterCounterLabel() {
