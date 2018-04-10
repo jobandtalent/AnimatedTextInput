83a671fd0fcb5e0094fc50c4929562b8cb7401fa
Herre <ikbenhansvanherreweghe@gmail.com>
2018-04-10 12:30:59 +0200

    send editingChanged action when editing

--- AnimatedTextInput/Classes/AnimatedTextInput.swift
+++ AnimatedTextInput/Classes/AnimatedTextInput.swift
@@ -531,6 +531,7 @@ extension AnimatedTextInput: TextInputDelegate {
 
     open func textInputDidChange(textInput: TextInput) {
         updateCounter()
+        sendActions(for: .editingChanged)
         delegate?.animatedTextInputDidChange?(animatedTextInput: self)
     }
 
